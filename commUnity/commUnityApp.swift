4bbf88d2610d6b56d836a2ab33773e9fcd570e83
Michelle Han <michellehan2007agt@gmail.com>
2023-10-17 21:28:41 -0700

    Initial Commit

---
+++ commUnity/commUnityApp.swift
@@ -0,0 +1,17 @@
+//
+//  commUnityApp.swift
+//  commUnity
+//
+//  Created by Michelle Han on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct commUnityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
